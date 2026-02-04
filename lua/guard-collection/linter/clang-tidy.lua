847319cee2670f4d522cfd892068a6f534f1f2d2
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-04 17:54:11 -0500

    revert: remove clang-analyzer flag and noautocmd

--- lua/guard-collection/linter/clang-tidy.lua
+++ lua/guard-collection/linter/clang-tidy.lua
@@ -2,7 +2,7 @@ local lint = require('guard.lint')
 
 return {
   cmd = 'clang-tidy',
-  args = { '--checks=clang-analyzer-*', '--quiet' },
+  args = { '--quiet' },
   fname = true,
   parse = lint.from_regex({
     source = 'clang-tidy',
--- test/lint_helper.lua
+++ test/lint_helper.lua
@@ -14,8 +14,8 @@ function M.test_with(ft, input)
   vim.bo[bufnr].filetype = ft
   api.nvim_set_current_buf(bufnr)
   api.nvim_buf_set_lines(bufnr, 0, -1, false, input)
-  -- To make linters happy (noautocmd prevents guard's BufWrite autocmd from triggering)
-  vim.cmd('silent! noautocmd write! /tmp/test.' .. ft)
+  -- To make linters happy
+  vim.cmd('silent! write! /tmp/test.' .. ft)
 
   lint.do_lint(bufnr)
   vim.wait(3000)
