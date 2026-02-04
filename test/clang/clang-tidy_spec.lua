f9ee2888c0f2ff5a1fd0ce1f2df6703b1fe1d0f0
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-04 15:14:31 -0500

    refactor(test): reorganize tests by package manager
    Move tests from test/{formatter,linter}/ to test/{pip,npm,go,rust,lua,binary,clang,haskell}/
    to allow CI to use glob patterns (busted test/pip/*_spec.lua) instead of
    listing each test file explicitly.
    
    This means adding new tools no longer requires workflow changes.

---
+++ test/clang/clang-tidy_spec.lua
@@ -0,0 +1,44 @@
+describe('clang-tidy', function()
+  it('can lint', function()
+    local helper = require('test.lint_helper')
+    local ns = helper.namespace
+    local ft = require('guard.filetype')
+    ft('c'):lint('clang-tidy')
+
+    local buf, diagnostics = helper.test_with('c', {
+      [[#include <stdio.h>]],
+      [[int main() {]],
+      [[    int x = 10;]],
+      [[    int y = 0;]],
+      [[    printf("%d", x / y);]],
+      [[    return 0;]],
+      [[}]],
+    })
+    assert.are.same({
+      {
+        bufnr = buf,
+        code = 'clang-analyzer-core.DivideZero',
+        col = 19,
+        end_col = 4,
+        end_lnum = 4,
+        lnum = 4,
+        message = 'Division by zero',
+        namespace = ns,
+        severity = 2,
+        source = 'clang-tidy',
+      },
+      {
+        bufnr = buf,
+        code = 'clang-analyzer-core.DivideZero',
+        col = 19,
+        end_col = 4,
+        end_lnum = 4,
+        lnum = 4,
+        message = 'Division by zero',
+        namespace = ns,
+        severity = 2,
+        source = 'clang-tidy',
+      },
+    }, diagnostics)
+  end)
+end)
---
+++ test/go/gofumpt_spec.lua
@@ -0,0 +1,27 @@
+describe('gofumpt', function()
+  it('can format', function()
+    local ft = require('guard.filetype')
+    ft('go'):fmt('gofumpt')
+
+    local formatted = require('test.fmt_helper').test_with('go', {
+      [[package         main]],
+      [[]],
+      [[     import   "fmt"     ]],
+      [[]],
+      [[func    main() {]],
+      [[   x:=     1;]],
+      [[          fmt.Println(x);]],
+      [[}]],
+    })
+    assert.are.same({
+      [[package main]],
+      [[]],
+      [[import "fmt"]],
+      [[]],
+      [[func main() {]],
+      [[	x := 1]],
+      [[	fmt.Println(x)]],
+      [[}]],
+    }, formatted)
+  end)
+end)
