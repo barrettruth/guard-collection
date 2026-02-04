bbaccbb0592f4d9a6fde8d539186770bfd3976c6
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-04 11:44:37 -0500

    fix: revert ci change

--- test/env.sh
+++ test/env.sh
@@ -2,4 +2,3 @@
 export PATH="/home/linuxbrew/.linuxbrew/opt/clang-format/bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-export PATH="$HOME/.luarocks/bin:$PATH"
