281de8e0c513a88acf1a6c44b1df8ee8bd535428
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-06 14:33:05 -0500

    fix(test): hadolint use file arg instead of stdin

--- test/binary/hadolint_spec.lua
+++ test/binary/hadolint_spec.lua
@@ -1,13 +1,26 @@
 describe('hadolint', function()
   it('can lint', function()
-    local helper = require('test.helper')
-    local buf, diagnostics = helper.run_lint('hadolint', 'dockerfile', {
+    local linter = require('test.helper').get_linter('hadolint')
+    local tmpfile = '/tmp/guard-test.dockerfile'
+    local input = {
       [[FROM ubuntu]],
       [[RUN apt-get install python]],
-    })
+    }
+    vim.fn.writefile(input, tmpfile)
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    local result = vim
+      .system({
+        'hadolint',
+        '--no-fail',
+        '--format=json',
+        tmpfile,
+      })
+      :wait()
+    local output = result.stdout or ''
+    local diagnostics = linter.parse(output, bufnr)
     assert.is_true(#diagnostics > 0)
     for _, d in ipairs(diagnostics) do
-      assert.equal(buf, d.bufnr)
+      assert.equal(bufnr, d.bufnr)
       assert.equal('hadolint', d.source)
       assert.is_number(d.lnum)
       assert.is_string(d.message)
