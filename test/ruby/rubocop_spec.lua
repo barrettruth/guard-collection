ebf68b524356d40342c692deb2d280e5a7e54c77
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-10 13:13:33 -0500

    fix(rubocop): use rubocop directly instead of bundle exec
    Problem: rubocop formatter and linter definitions used
    `bundle exec rubocop`, requiring a Gemfile and bundle install
    in both tests and CI.
    
    Solution: invoke rubocop directly via `gem install rubocop`.
    Remove Gemfile setup from test and CI.

--- test/ruby/rubocop_spec.lua
+++ test/ruby/rubocop_spec.lua
@@ -1,20 +1,9 @@
 describe('rubocop', function()
-  local tmpdir = '/tmp/rubocop-test'
-
-  setup(function()
-    vim.fn.mkdir(tmpdir, 'p')
-    vim.fn.writefile({
-      "source 'https://rubygems.org'",
-      "gem 'rubocop'",
-    }, tmpdir .. '/Gemfile')
-    vim.system({ 'bundle', 'install' }, { cwd = tmpdir }):wait()
-  end)
-
   it('can lint', function()
     local helper = require('test.helper')
     local bufnr, diagnostics = helper.run_lint('rubocop', 'rb', {
       [[x = {  :a=>1,:b  =>  2  }]],
-    }, { cwd = tmpdir })
+    })
     assert.is_true(#diagnostics > 0)
     helper.assert_diag(diagnostics[1], {
       bufnr = bufnr,
